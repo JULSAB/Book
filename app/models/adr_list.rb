ad7e609d2a2f9dabe787df1cde52795683335c58
JULSAB <julius.sabaliauskas@gmail.com>
2021-03-21 12:20:15 +0200

    Added validation for fields and browser popup for empty or bad fiilled fields

--- app/models/adr_list.rb
+++ app/models/adr_list.rb
@@ -1,2 +1,9 @@
 class AdrList < ApplicationRecord
+
+    validates :Address, presence: true
+    validates :Phone, presence: true
+    validates :Name, presence: true
+    validates :LastName, presence: true
+    validates :email, presence: true
+
 end
