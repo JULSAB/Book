5021698e80976017436a92dd613175aba87b7037
JULSAB <julius.sabaliauskas@gmail.com>
2021-03-21 12:21:51 +0200

    phone data type changed to string. This still not help to update phone views field

--- db/migrate/20210321085716_create_adr_lists.rb
+++ db/migrate/20210321085716_create_adr_lists.rb
@@ -2,7 +2,7 @@ class CreateAdrLists < ActiveRecord::Migration[6.1]
   def change
     create_table :adr_lists do |t|
       t.string :Address
-      t.integer :Phone
+      t.string :Phone
       t.string :Name
       t.string :LastName
       t.string :email
