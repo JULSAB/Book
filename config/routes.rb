eba8c738b767cd4c21d29338907bef255449f593
JULSAB <julius.sabaliauskas@gmail.com>
2021-03-21 11:12:48 +0200

    Updated root to show adrlist in localhost server

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :adr_lists
+  root 'adr_lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
